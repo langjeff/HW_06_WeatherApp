0670e4f4adf564127708214f3d9b0c7701b39112
IronJon <lang.jeffrey@gmail.com>
2020-10-30 18:26:12 -0600

    restyled element in html file

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=denver&appid=18c8e29c511
   .then(function (data) {
     console.log(data);
   });
+  //current forecast
+  // city name, date, an icon w/ conditions, temperature, humidity, wind speed and uv index ( use css styles and class for color coding for favorable, normal, unfavorable)
 
+// http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+
+  //future 5 day forecast
+  //date, icon for conditions, temperature, and humidity
+  // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+  
 
 //   18c8e29c5116333e08aef89074ae53f7
