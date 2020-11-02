9ce47e383dc00e6dc87ff306fdc607548465b773
IronJon <lang.jeffrey@gmail.com>
2020-11-01 19:41:02 -0700

    finished card elements for five day forecast

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ var windEl = document.getElementById("wind");
 var uvEl = document.getElementById("uv");
 var minEl = document.getElementById("min");
 var maxEl = document.getElementById("max");
+var fiveDayEl = document.getElementById("fiveDay");
 
 // get current weather from openweathermap
-fetch('http://api.openweathermap.org/data/2.5/weather?q=denver&appid=18c8e29c5116333e08aef89074ae53f7&units=imperial')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=denver&appid=18c8e29c5116333e08aef89074ae53f7&units=imperial')
   .then(function (response) {
     return response.json();
   })
@@ -51,54 +52,94 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=denver&appid=18c8e29c511
     // wind speed
     console.log(data.wind.speed);
     windEl.textContent = "Wind Speed: " +data.wind.speed + " mph";
-    // max temp
-    console.log(data.main.temp_max);
-    maxEl.textContent = "Max. Temp. " +data.main.temp_max + "\xb0";
-    // min temp
-    console.log(data.main.temp_min);
-    minEl.textContent = "Min. Temp. " +data.main.temp_min + "\xb0";
+
     // coordinates data to pass to 5 day and uv
     console.log(data.coord);
     long = data.coord.lon;
     console.log(long);
     lat = data.coord.lat;
     console.log(lat);
+      // nested fetch for uv index
+      var uvUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + long + '&appid=18c8e29c5116333e08aef89074ae53f7';
+      fetch(uvUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        console.log(data.value);
+        uvEl.textContent = "UV Index: " + data.value;
+      });
+      //nested fetch for 5 day forecast
+      var fiveDayUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=imperial&exclude=current,minutely,hourly,alerts&appid=18c8e29c5116333e08aef89074ae53f7';
+      fetch(fiveDayUrl)
+      .then(function (response) {
+      return response.json();
+      })
+      .then(function (data) {
+
+      console.log(data);
+      for (var i = 1; i < 6; i++) {
+        // create card for five day forecast
+      var fiveDayCard = document.createElement("div");
+      fiveDayCard.setAttribute("class","card d-inline-flex p-2 m-1")
+      fiveDayCard.setAttribute("style","width: 15rem;");
+      fiveDayEl.appendChild(fiveDayCard);
+        //create element for image icon
+      var fiveDayIcon = document.createElement("img");
+      fiveDayIcon.setAttribute("class","card-img-top text-center");
+      fiveDayIcon.setAttribute("src","http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+      fiveDayIcon.setAttribute("style","width: 100px;");
+      fiveDayIcon.setAttribute("alt","weather icon");
+      fiveDayCard.appendChild(fiveDayIcon);
+      // console.log(data.daily[i].weather[0].icon);
+        //create card body
+      var fiveDayBody = document.createElement("div");
+      fiveDayBody.setAttribute("class","card-body");
+      fiveDayCard.appendChild(fiveDayBody);
+        //fiveday date
+      var fiveDayDate = (data.daily[i].dt);
+        // converts date to string 
+      var fiveDayDatems = fiveDayDate * 1000;
+      var fiveDayDateObject = new Date(fiveDayDatems);
+      var fiveDayDateString = fiveDayDateObject.toLocaleDateString();
+      var fiveDayDateDay = fiveDayDateObject.toLocaleString("en-US", {weekday: "long"});
+        // create card title
+      var fiveDayTitle = document.createElement("h5");
+      fiveDayTitle.setAttribute("class","card-title text-center")  
+      fiveDayTitle.textContent = fiveDayDateDay;
+      fiveDayBody.appendChild(fiveDayTitle);
+        // create card text date
+      var fiveDayTextDate = document.createElement("p");
+      fiveDayTextDate.setAttribute("class","card-text text-center");
+      fiveDayTextDate.textContent = fiveDayDateString;  
+      fiveDayBody.appendChild(fiveDayTextDate);
+      // console.log(fiveDayDateString);
+      // console.log(fiveDayDateDay);
+        // create temperature for card
+      var fiveDayTemp = document.createElement("p");
+      fiveDayTemp.setAttribute("class","card-text text-center");
+      fiveDayTemp.textContent = "Temp: " + data.daily[i].temp.day + "\xb0";
+      fiveDayBody.appendChild(fiveDayTemp);  
+      // console.log(data.daily[i].temp.day);
+        //create humidity for card
+      var fiveDayHumidity = document.createElement("p");
+      fiveDayHumidity.setAttribute("class","card-text text-center");
+      fiveDayHumidity.textContent = "Humid.: " + data.daily[i].humidity +"%";
+      fiveDayBody.appendChild(fiveDayHumidity);
+      //  console.log(data.daily[i].humidity);
+        //create weather description for card  
+      var fiveDayDescription = document.createElement("p");
+      fiveDayDescription.setAttribute("class","card-text text-center");
+      fiveDayDescription.textContent = data.daily[i].weather[0].description;
+      fiveDayBody.appendChild(fiveDayDescription);
+      }
+      });
   });
 
-  //current forecast
-  // city name, date, an icon w/ conditions, temperature, humidity, wind speed and uv index ( use css styles and class for color coding for favorable, normal, unfavorable)
 
-   //uv index
-  var uvUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + long + '&appid=18c8e29c5116333e08aef89074ae53f7';
-  fetch(uvUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.value);
-    uvEl.textContent = "UV Index: " + data.value;
-  });
  
-  //5 day forecast
-  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=39.74&lon=-104.98&units=imperial&exclude=current,minutely,hourly,alerts&appid=18c8e29c5116333e08aef89074ae53f7')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    for (var i = 1; i < 6; i++) {
-    var date = (data.daily[i].dt);
-    // console.log(date);
-    var datems = date * 1000;
-    var dateObject = new Date(datems);
-    var dateString = dateObject.toLocaleDateString();
-    console.log(dateString);
-    console.log(data.daily[i].temp.day);
-    console.log(data.daily[i].weather[0].icon);
-    console.log(data.daily[i].humidity);
-  }
-});
+
 
 // // icon link
 // http://openweathermap.org/img/wn/10d@2x.png
